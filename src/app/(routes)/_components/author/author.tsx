b0626d41676d9d5ffa7c387782236b0b04529ea1
frikk - tronderenergi <frikk.a.odlolien@tietoevry.com>
2024-12-11 08:51:56 +0100

    fixes api issues

--- src/app/(routes)/_components/author/author.tsx
+++ src/app/(routes)/_components/author/author.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
 import Image from "next/image";
+import { cn } from "@/utils/cn";
 
 type Author = {
   id: number;
   login: string;
   avatar_url: string;
-  profileUrl: string;
+  url: string;
 };
 
 interface AuthorProps {
@@ -18,7 +19,7 @@ const Author = ({ authors, githubUsername }: AuthorProps) => {
     if (contributor.login === githubUsername) {
       return {
         id: contributor.id,
-        profileUrl: contributor.profileUrl,
+        url: contributor.url,
         avatar_url: contributor.avatar_url,
         login: contributor.login,
       };
@@ -34,8 +35,14 @@ const Author = ({ authors, githubUsername }: AuthorProps) => {
   return (
     <div>
       Author:
-      <Link href={mappedAuthor.profileUrl}>
-        <Image src={mappedAuthor.avatar_url} alt="Author image" />
+      <Link href={mappedAuthor.url}>
+        <Image
+          className={cn("rounded-full border-2 border-white-100")}
+          src={mappedAuthor.avatar_url}
+          width={32}
+          height={32}
+          alt="Author image"
+        />
       </Link>
     </div>
   );
